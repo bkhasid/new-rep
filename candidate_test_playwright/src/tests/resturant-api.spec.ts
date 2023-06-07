001a03fa767a4b4863b3788d4a2424b41e49f814
Tzahi Anidgar <tzahi@testomate.io>
2023-06-07 14:27:34 +0300

    add random values

--- candidate_test_playwright/src/tests/resturant-api.spec.ts
+++ candidate_test_playwright/src/tests/resturant-api.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { BrowseWrapper } from '../infra/browser/browser';
 import restaurantsAPI from '../logic/api/restaurantsAPI';
+import { generateRandomString, getRandomInt } from '../infra/utils';
 
 test.describe('Base API test', () => {
 
@@ -20,11 +21,11 @@ test.describe('Base API test', () => {
 
     test('Create restaurants via api', async () => {
         //Arrange
-        const myNewRest = { address: "My Addess 1", id: 233, name: "My Restaurant", score: 2.3 }
+        const myNewRest = { address: generateRandomString(10), id: getRandomInt(999, 100), name: generateRandomString(5), score: getRandomInt(99, 1) }
         await restaurantsAPI.createRestaurant(myNewRest);
 
         //Act
-        const getByIdResponse = await restaurantsAPI.getRestaurantById(233);
+        const getByIdResponse = await restaurantsAPI.getRestaurantById(myNewRest.id);
 
         //Assert
         expect(getByIdResponse.status()).toEqual(201)
